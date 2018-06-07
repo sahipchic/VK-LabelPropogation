007dfe4e182281769774da4ab233266ad287e5b6
Ilya <ilja.sahipoff@yandex.ru>
2018-06-07 18:50:42 +0300

    added connection

--- src/Node.java
+++ src/Node.java
@@ -8,23 +8,29 @@ public class Node {
     private int id;
     private int label;
     private Set<Integer> neighboors;
+
     public Node(int id, int label){
         this.id = id;
         this.label = label;
         this.neighboors = new HashSet<>();
     }
+
     public int getId(){
         return this.id;
     }
+
     public int getLabel(){
         return this.label;
     }
+
     public void setLabel(int label){
         this.label = label;
     }
+
     public Set<Integer> getNeighboors(){
         return this.neighboors;
     }
+
     public void addNeighboor(int id){
         this.neighboors.add(id);
     }
